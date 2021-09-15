60c3c2f793ee6e9120c805f8d7ca29532d19c633
Florencia Amendola <amendolaflorencia@gmail.com>
2021-09-15 11:33:04 -0300

    operadores

--- variables/app.js
+++ variables/app.js
@@ -11,11 +11,41 @@
 // Suma
 var primerNumero = 15000;
 var segundoNumero = 545646;
-var resultado = primerNumero + segundoNumero;
+var resultado = primerNumero * segundoNumero;
 console.log("Tu resultado es: " + resultado);
 
 // Nombre
-var primerNombre = "Maria";
-var segundoNombre = "Florencia";
+var primerNombre = "Carlos";
+var segundoNombre = "German";
 var nombreCompleto = primerNombre + " " + segundoNombre;
 console.log(nombreCompleto);
+
+// Tipos de datos
+/*
+Numericos: INT (enteros) = 1 o FLOAT (flotantes) = 1.5,
+Texto: Strings (Alfanumericas) = "Esto es un texto", "152546",
+*/
+
+// Operadores Aritmeticos
+/*
++ = suma
+- = resta
+/ = division
+* = multiplicación
+/// Operadores extendidos
+% = resto
+%% = modulo
+*/
+
+// Funcion Math
+var numeroExperimental = 10;
+console.log(Math.pow(numeroExperimental, 3));
+console.log(Math.round(1.77777777777));
+console.log(Math.round(Math.random()*1000));
+
+// Numeros aleatorios limitados
+function numeroAleatorioVectorizado(min, max){
+    console.log(Math.round((Math.random() * (max - min) + min)))
+}
+
+numeroAleatorioVectorizado(10, 100);
