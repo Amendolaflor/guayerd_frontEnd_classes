4f6f672c21dc7270654f3d535ca8c59c1e8be1e5
Florencia Amendola <amendolaflorencia@gmail.com>
2021-09-17 11:23:32 -0300

    ultimos cambios

--- variables/main.js
+++ variables/main.js
@@ -1,8 +1,8 @@
 
-//////////////////     Ejercicios con variables y operadores       ///////////////
+// //////////////////     Ejercicios con variables y operadores       ///////////////
 
 
-////////////// 0
+// ////////////// 0
 let nombre = window.prompt("Ingrese nombre");
 let apellido = window.prompt("Ingrese apellido");
 
@@ -10,7 +10,7 @@ console.log( "Hola" + " " + nombre + " " + apellido);
 alert("Hola" + " " + nombre + " " + apellido);
 
 
-///////////// 1
+// ///////////// 1
 let primerNumero = parseInt(prompt("Ingrese un numero"));
 let segundoNumero = parseInt(prompt("Ingrese otro numero"));
 let resultado = primerNumero + segundoNumero;
@@ -19,7 +19,7 @@ console.log("La suma de tus numero es:" + " " + resultado);
 alert( "La suma de tus numeros es:" + " " + resultado);
 
 
-/////////// 2
+// /////////// 2
 let edad = parseInt(window.prompt("Cuantos años cumplio o cumple este año?"));
 let añoActual = 2021;
 let añoNacimiento = añoActual - edad;
@@ -28,7 +28,7 @@ console.log("Naciste en el año:" + " " + añoNacimiento)
 alert("Naciste en el año:" + " " + añoNacimiento);
 
 
-///////////// 3
+// ///////////// 3
 let balanceCuenta = 80000;
 
 alert("Tu balance de cuenta es:" + " " + balanceCuenta);
@@ -40,7 +40,7 @@ console.log("Tu saldo actual es de:" + " " + saldoRestante)
 alert("Tu saldo actual es de:" + " " + saldoRestante);
 
 
-////////////// 4
+// ////////////// 4
 let notaPrimerTrimestre = parseInt(window.prompt("Cual fue tu nota final en el primer trimestre?"));
 let notaSegundoTrimestre = parseInt(window.prompt("Y en el segundo trimestre?"));
 let notaTercerTrimestre = parseInt(window.prompt("Por ultimo necesito la del tercero para calcular tu promedio"));
@@ -50,7 +50,7 @@ console.log("Tu nota final es:" + " " + notaFinal)
 alert("Tu nota final es:" + " " + notaFinal);
 
 
-///////////// 5
+// ///////////// 5
 let NumeroEj5 = parseInt(prompt("Ingrese un numero"));
 let pies = NumeroEj5 / 40
 let centimetros = NumeroEj5 * 100
@@ -62,7 +62,7 @@ alert("Tu numero convertido a pies es:" + " " + pies  + "\n" +
 console.log("Tu numero convertido a pies es:" + " " + pies, "Tu numero convertido a centimetros es:" + " " + centimetros, "Tu numero convertido a pulgadas es:" + " " + pulgadas,);
 
 
-///////////// 6
+// ///////////// 6
 let primerNumeroEj6 = parseInt(prompt("Ingrese un numero"));
 let segundoNumeroEj6 = parseInt(prompt("Ingrese otro numero"));
 let resultadoEj6 = primerNumeroEj6 + segundoNumeroEj6
@@ -94,7 +94,7 @@ console.log("El area de tu rectangulo es:" + " " + resultadoRectangulo);
 alert( "El area de tu rectangulo es:" + " " + resultadoRectangulo);
 
 
-///////////// 9
+// ///////////// 9
 let numeroBaseEquilatero = parseInt(prompt("Ingrese un numero"));
 let numeroAlturaEquilatero = parseInt(prompt("Ingrese otro numero"));
 let resultadoTrianguloEquilatero = (numeroBaseEquilatero *  numeroAlturaEquilatero) / 2;
@@ -103,7 +103,7 @@ console.log("El area de tu triangulo equilatero es:" + " " + resultadoTrianguloE
 alert( "El area de tu triangulo equilatero es:" + " " + resultadoTrianguloEquilatero);
 
 
-///////////// 10
+// ///////////// 10
 let numeroEj10 = parseInt(prompt("Ingrese un precio"));
 let numeroDescuento = parseInt(prompt("Ingrese un numero para descontar"));
 let resultadoPorcentaje = (numeroEj10 * numeroDescuento) / 100; 
@@ -113,7 +113,7 @@ console.log("El precio con descuento es $:" + " " + resultadoConDescuento);
 alert("El precio con descuento es $:" + " " + resultadoConDescuento);
 
 
-///////////// 11
+// ///////////// 11
 let nombreEj11 = window.prompt("Ingrese nombre");
 let edadEj11 = parseInt(window.prompt("Cuantos años cumplio o cumple este año?"));
 let añoActualEj11 = 2021;
@@ -123,7 +123,7 @@ console.log("Hola" + " " + nombreEj11 + " " + "naciste en el año" + añoNacimie
 alert("Hola" + " " + nombreEj11 + " " + "naciste en el año" + añoNacimientoEj11);
 
 
-///////////// 12
+// ///////////// 12
 let nombreEj12 = window.prompt("Ingrese nombre");
 let temperatura = parseInt(window.prompt("Ingrese la temperatura del dia"));
 let temperaturaFarenheit = (temperatura * (9/5)) + 32;
@@ -132,7 +132,7 @@ console.log(nombreEj12 + ": " + "la tempertura ingresada en Farenheit es:" + " "
 alert(nombreEj12 + ": " + "la tempertura ingresada en Farenheit es:" + " " + temperaturaFarenheit);
 
 
-///////////// 13
+// ///////////// 13
 let nombreEj13 = window.prompt("Ingrese nombre");
 let temperaturaEj13 = parseInt(window.prompt("Ingrese la temperatura del dia"));
 let temperaturaCelsius = Math.round((temperaturaEj13 - 32) * (5/9));
@@ -141,7 +141,7 @@ console.log(nombreEj13 + ": " + "la tempertura ingresada en Celsius es:" + " " +
 alert(nombreEj13 + ": " + "la tempertura ingresada en Celsuis es:" + " " + temperaturaCelsius);
 
 
-/////////// 14
+// /////////// 14
 let numeroEj14 = parseInt(window.prompt("Ingrese numero"));
 let sumaEj14 = numeroEj14 + 5;
 let multiplicacionEj14 = sumaEj14 * 10;
@@ -155,8 +155,7 @@ console.log(multiplicacionEj14);
 console.log(restoEj14);	
 
 
-
-////////////// 0  BONUS
+// ////////////// 0  BONUS
 let primerNumeroString = window.prompt("Ingrese un numero");
 let segundoNumeroString = window.prompt("Ingrese otro numero");
 let numerosJuntos = primerNumeroString + segundoNumeroString
@@ -166,10 +165,30 @@ alert("La union de tus numero es:" + " " + numerosJuntos);
 
 
 ////////////// 1  BONUS
-let años = parseFloat(prompt("Ingresa un numero de años"));
+let años = parseInt(prompt("Ingresa un numero de años"));
 let segundosxDia = (60 * 60) * 24;
 let resultadoDiasxAños = años * 365;
 let resultadoSegxDia = años * segundosxDia;
 
 console.log(`La cantidad de años ingresados equivale a: ${resultadoDiasxAños} dias y a ${resultadoSegxDia} segundos.`);
 alert(`La cantidad de años ingresados equivale a: ${resultadoDiasxAños} dias y a ${resultadoSegxDia} segundos.`);
+
+////////////// 2  BONUS
+let cantidadDeKilometrosARecorrer = parseFloat(window.prompt("Ingrese distancia en Km"));
+let velocidadPromedio = parseFloat(window.prompt(" Ingrese velocidad promedio en Km/h "));
+let valorKmPorMin = velocidadPromedio / 60;
+let tiempoEstimadoEnMinutos = Math.round(cantidadDeKilometrosARecorrer / valorKmPorMin) ;
+
+console.log(tiempoEstimadoEnMinutos);
+alert ("El tiempo en minutos es:" + " " + tiempoEstimadoEnMinutos);
+
+
+////////////////////// 3 BONUS 
+let bruto = parseInt(window.prompt("Ingrese sueldo bruto"));
+let obraSocial = (bruto * 3) / 100;
+let ley = (bruto * 11) / 100;
+let jubilacion = (bruto * 3) / 100;
+let neto = bruto - (obraSocial + ley + jubilacion); 
+
+alert(`Cobras en mano ${neto}`)
+console.log(neto);
