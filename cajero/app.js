c07ada73332a43f32bfe10b072ba79a91e16bc98
Florencia Amendola <amendolaflorencia@gmail.com>
2021-09-22 01:10:39 -0300

    funcionalidades

--- cajero/app.js
+++ cajero/app.js
@@ -5,14 +5,41 @@ function login() {
 	let passReg = document.getElementById("passwordRegister").value;
 	let userLog = document.getElementById("userNameLogin").value;
 	let passLog = document.getElementById("passwordLogin").value;
-	
-	if(userLog === userReg && passReg === passLog){
+
+	if (userLog === userReg && passReg === passLog) {
 		window.location.replace("./menu.html")
 	} else {
 		alert(`Usuario o Contraseña incorrecto.`)
-	}	
+	}
 }
 
 
+///////////// menu princial ////////////////////////
 
+///// fake data ---------------------
+let userSaldo = 80000
+
+
+function verSaldo() {
+	alert(`Tu saldo actual es de: $${userSaldo}`);	
+}
+
+
+function retirarDinero() {
+	let extraccion = parseInt(prompt("¿Cuánto dinero desea retirar?"));
+	alert(`EXTRACCION RELIZADA CON EXITO. \n Tu saldo actual es de ${userSaldo - extraccion}`)	
+}
+
+
+function depositarDinero() {
+	let deposito = parseInt(prompt("¿Cuánto dinero desea depositar?"));
+	alert(`DEPOSITO RELIZADO CON EXITO. \n Tu saldo actual es de ${userSaldo + deposito}`);	
+}
+
+
+function cambiarPassword() {	
+	let newPass = prompt(`ingrese contraseña nueva`);
+	alert(`CLAVE MODIFICADA CON EXITO`);
+	console.log(newPass)	
+}
 
